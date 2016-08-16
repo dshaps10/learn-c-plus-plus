b37954ed5f563bb2c63a97692fe08d0478ca06b7
dshaps10 <dan.shapiro1210@gmail.com>
2016-08-16 15:35:54 -0700

    Add summing functionality

--- soloLearn/UserInput/main.cpp
+++ soloLearn/UserInput/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int a, b;
+	int sum;
 	cout << "Please enter a number \n";
 	cin >> a;
-	cout << "Enter another number \n"
+	cout << "Enter another number \n";
 	cin >> b;
+	sum = a + b;
+	cout << "Sum is: " << sum << endl;
 	return 0;
 }
